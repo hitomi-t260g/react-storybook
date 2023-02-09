9db36bdcc934945e8f06c69851794d285782211b
hitomi-t260g <s_t_u_d_i_a@yahoo.co.jp>
2023-02-09 22:32:28 +0900

    fix primary- small button

--- src/components/Button/Button.stories.jsx
+++ src/components/Button/Button.stories.jsx
@@ -1,4 +1,4 @@
-import Button from './Button';
+import Button from '../Button/Button';
 
 export default {
   title: 'Common/Button',
@@ -6,5 +6,15 @@ export default {
 };
 
 export const Default = () => <Button>Default</Button>;
-export const Primary = () => <Button color="primary">Primary</Button>;
 export const Danger = () => <Button color="danger">Danger</Button>;
+export const Primary = () => <Button color="primary">Primary</Button>;
+export const PrimarySmall = () => (
+  <Button size="sm" color="primary">
+    Small
+  </Button>
+);
+export const PrimaryLarge = () => (
+  <Button size="lg" color="primary">
+    Large
+  </Button>
+);
