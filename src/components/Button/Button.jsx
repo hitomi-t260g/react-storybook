098b33a1ccb2b3c9b7f16d525fac0dd471d57cbb
hitomi-t260g <s_t_u_d_i_a@yahoo.co.jp>
2023-02-12 07:18:56 +0900

    add argTypes on Button.stores.jsx

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,8 +1,28 @@
 import './button.css';
+// import PropTypes from 'prop-types';
 
-function Button({ children, color = 'default', size = 'base' }) {
-  return <button className={`${color} ${size}`}>{children}</button>;
+function Button({
+  children,
+  color = 'default',
+  size = 'base',
+  backgroundColor
+}) {
+  return (
+    <button
+      className={`${color} ${size}`}
+      style={backgroundColor && {backgroundColor}}
+    >
+      {children}
+    </button>
+  );
 }
 
 
 export default Button;
+
+//colorやsizeなどをブラウザで選択式にするため、型にはめる
+//argsTypeを使う場合は不要
+// Button.propTypes = {
+//   color: PropTypes.oneOf(['primary', 'default', 'danger']),
+//   size : PropTypes.oneOf(['sm', 'base','lg'])
+// };
--- src/components/Button/Button.stories.jsx
+++ src/components/Button/Button.stories.jsx
@@ -3,18 +3,49 @@ import Button from '../Button/Button';
 export default {
   title: 'Common/Button',
   component: Button,
+  argTypes: {
+    color: {
+      options:['primary', 'default', 'danger'],
+      control:{type: 'radio'},
+    },
+    size: {
+      options: ['sm', 'base','lg'],
+      control:{type:'select'}
+    },
+    backgroundColor: {
+      control:{type:'color'}
+    }
+  },
 };
 
-export const Default = () => <Button>Default</Button>;
-export const Danger = () => <Button color="danger">Danger</Button>;
-export const Primary = () => <Button color="primary">Primary</Button>;
-export const PrimarySmall = () => (
-  <Button size="sm" color="primary">
-    Small
-  </Button>
-);
-export const PrimaryLarge = () => (
-  <Button size="lg" color="primary">
-    Large
-  </Button>
-);
+const Template = (args) => <Button {...args} />;  //ここの...argsとbindの流れがわからない
+export const Default =  Template.bind({});
+Default.args =  {
+  children: 'Default',
+};
+
+export const Danger = Template.bind({});
+Danger.args = {
+  children: 'Danger',
+  color: 'danger',
+}
+
+export const Primary =  Template.bind({});
+Primary.args = {
+  children: 'Primary',
+  color: 'primary',
+}
+
+export const PrimarySmall = Template.bind({});
+PrimarySmall.args = {
+  children: 'Small',
+  color: 'primary',
+  size: 'sm',
+}
+export const PrimaryLarge =  Template.bind({});
+PrimaryLarge.args = {
+  ...Primary.args,  //...PrimarySmall.argsなど指定し、同じargsを指定する。違うものは下に記述し、上書きする。
+  children: 'Large',
+  size: 'lg',
+}
+
